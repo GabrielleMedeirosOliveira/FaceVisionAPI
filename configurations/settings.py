e21a14264daabe6c57c3d4343478d93d667673a9
GabrielleMedeirosOliveira <gabriellemedeiros51@gmail.com>
2023-07-10 21:13:11 -0300

    feat: configure deploy on fly.dev

--- configurations/settings.py
+++ configurations/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hqphdclh_n98u^*vkh-jd1etfttg^rbqkl%q-i220i8di8h-t4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SWAGGER_SETTINGS = {
+   'USE_SESSION_AUTH': False
+}
