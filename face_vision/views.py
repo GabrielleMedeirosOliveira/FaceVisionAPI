69ba1f9a7cb40876960a00c3fae8ae3aac70de24
GabrielleMedeirosOliveira <gabriellemedeiros51@gmail.com>
2023-07-11 21:13:39 -0300

    feat: saves the comparison results in base64 and creates a JSON file

--- face_vision/views.py
+++ face_vision/views.py
@@ -5,6 +5,7 @@ import face_recognition as fr
 import requests
 import json
 import cv2
+import base64
 import os
 
 
@@ -14,14 +15,29 @@ def processing(request):
         body = request.body
         body_str = body.decode('utf-8')
         json_data = json.loads(body_str)
-        
+
         image_url = json_data['image_url']
         file_name = json_data["file_name"]
-        save_image_from_url(image_url,"./directory",file_name)
-        facial_recognition(file_name)
+        save_image_from_url(image_url, "./directory", file_name)
+        comparison_result = facial_recognition(file_name)
+
+        # Convert images to base64
+        img1_base64 = image_to_base64("./directory/{}".format(file_name))
+        img2_base64 = image_to_base64("./directory/Jonh Travolta.jpg")
+
+        # Create dictionary to store data
+        data = {
+            'image1': img1_base64,
+            'image2': img2_base64,
+            'comparison_result': str(comparison_result)
+        }
+
+        # Save data to JSON file
+        with open('result.json', 'w') as json_file:
+            json.dump(data, json_file)
 
         return HttpResponse(status=200)
-
+    
 def save_image_from_url(image_url, save_directory, file_name):
     response = requests.get(image_url)
     
@@ -38,7 +54,7 @@ def save_image_from_url(image_url, save_directory, file_name):
 def facial_recognition(file_name):
     image_by_request = fr.load_image_file("./directory/{}".format(file_name))
     image_by_request = cv2.cvtColor(image_by_request,cv2.COLOR_BGR2RGB)
-    image_by_directory = fr.load_image_file("./directory/Elon-Musk.jpg")
+    image_by_directory = fr.load_image_file("./directory/Jonh Travolta.jpg")
     image_by_directory = cv2.cvtColor(image_by_directory,cv2.COLOR_BGR2RGB)
 
     face_localization = fr.face_locations(image_by_request)[0]
@@ -52,5 +68,12 @@ def facial_recognition(file_name):
 
     comparison_result = fr.compare_faces([encode_by_request], encode_by_directory)
 
+    cv2.waitKey(0) 
     print(comparison_result)
-    return cv2.waitKey(0)
+    return comparison_result
+      
+
+def image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+        return encoded_string
